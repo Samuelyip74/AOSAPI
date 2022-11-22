97f8141f237758002e133f8069922149bdfdf428
Samuel <samuel.yip@live.com>
2022-11-22 20:02:25 +0800

    commt

--- OVCirrusAPITesting.py
+++ OVCirrusAPITesting.py
@@ -8,15 +8,6 @@ disable_warnings(InsecureRequestWarning)
 
 OVnodeURL = 'https://eu.manage.ovcirrus.com/api/ov/v1/applications/authenticate'
 
-headers = { 'Content-Type' :  'application/json; charset=utf-8' }                    
-
-payload = {
-   'email'      :   'samuel.yip@al-enterprise.com',
-   'password'   :   'Ciscotac@123',
-   'appId'      :   '637c83ada1a19baa83c664c3',
-   'appSecret'  :   '01fac5723d2dc2b1cb5a4f448f1f36064fa81d2ccf8d1df79898a385285d240f'
-}
-
 ov = OVNodeAPI.OVConnection(
     email='samuel.yip@al-enterprise.com',
     password='Ciscotac@123',
@@ -26,6 +17,22 @@ ov = OVNodeAPI.OVConnection(
     debug=True
     )
 
+    
+# ov = OVNodeAPI.OVConnection(
+#     email='samuel.yip@al-enterprise.com',
+#     password='Ciscotac@123',
+#     hostaddress='apac.manage.ovcirrus.com',
+#     appId='637cacd9f61066758aec97ef',
+#     appSecret='037f0757ef65a65ad99988ad4e68c926b645f6bad3a86f55fb9baaca9d22e5f5',
+#     debug=True
+#     )
+
 status, data = ov.login()
 print(status, data)
+
+#https://manage.ovcirrus.com/api/ov/v1/organizations
+status, data = ov.APIRequest('api/ov/v2/user/profile')
+print(status, data)
+
+
 
--- OVNodeAPI.py
+++ OVNodeAPI.py
@@ -22,6 +22,7 @@ class OVConnection(object):
         self.useport     = useport
         self.debug       = debug
         self.token       = None
+        self.expires_in  = None
         self.token_type  = None
 
     def endpoint(self):
@@ -29,15 +30,52 @@ class OVConnection(object):
         return "%s://%s%s/" % (("http", "https")[self.secure == True], self.hostaddress, ('', ':' + str(self.useport))[str(self.useport) != '-1'])
 
     def login(self):
-        # login endpoint https:// {hostaddress} /api/ov/v1/applications/authenticate
-        authenticate_endpoint = '/api/ov/v1/applications/authenticate'
-        headers = { 'Content-Type' :  'application/json; charset=utf-8' }  
+        # login endpoint https://{hostaddress}/api/ov/v1/applications/authenticate
+        authenticate_endpoint = 'api/ov/v1/applications/authenticate'
+        header = { 'Content-Type' :  'application/json; charset=utf-8' }  
         payload = {
         'email'      :   self.email,
         'password'   :   self.password,
         'appId'      :   self.appId,
         'appSecret'  :   self.appSecret
         }
-        req = requests.post(self.endpoint() + authenticate_endpoint, json=payload, headers=headers)
-        return req.status_code, req.json()
+        req = requests.post(self.endpoint() + authenticate_endpoint, json=payload, headers=header, verify=False)
+        try:
+            if req.status_code == 200:
+                data = req.json()
+                self.token_type = data['token_type']
+                self.token = data['access_token']
+                self.expires_in = data['expires_in']
+                return req.status_code, data
+        except:
+            return 500, None
+
+    def APIRequest(self, endpoint):
+        header = { 
+            'Content-Type' :  'application/json; charset=utf-8',
+            'Authorization' : self.token_type + ' ' + self.token
+            }
+
+        try:
+            req = requests.post(self.endpoint() + endpoint, headers=header, verify=False)    
+            if req.status_code == 200:
+                return req.status_code, req.json()
+                
+            if req.status_code == 404:
+                return req.status_code, None     
+
+            else:
+                return req.status_code, None 
+        except:
+            return 500, None   
+
+
+    def getUserProfile(self):
+        # login endpoint https://{hostaddress}/api/ov/v1/user/profile
+        userProfile_endpoint = '/api/ov/v1/user/profile'
+
+        status, data = self.APIRequest(userProfile_endpoint)
+        return status, data
+
+
 
