23c413083bfbcc23ef2b6064c8ddfcae0d4783f4
Samuel <samuel.yip@live.com>
2023-03-24 21:28:11 +0800

    commit

--- ovna_test.py
+++ ovna_test.py
@@ -1,6 +1,6 @@
 import pysyslogclient
 
-client = pysyslogclient.SyslogClientRFC5424("192.168.28.26", "10514", proto="UDP")
+client = pysyslogclient.SyslogClientRFC5424("192.168.28.28", "10514", proto="UDP")
 
 client.log("DESKTOP-MGFHUAM login, ips, botnet, attack",
 	facility=pysyslogclient.FAC_SYSTEM,
