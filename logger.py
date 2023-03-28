f609b6538ac6500be249f0283ce5330a133b5537
Samuel <samuel.yip@live.com>
2023-03-28 18:06:13 +0800

    loggin

--- logger.py
+++ logger.py
@@ -2,6 +2,6 @@ import logging
 import logging.handlers
 my_logger = logging.getLogger('MyLogger')
 my_logger.setLevel(logging.DEBUG)
-handler = logging.handlers.SysLogHandler(address = ('192.168.28.31',10514))
+handler = logging.handlers.SysLogHandler(address = ('192.168.28.34',10514))
 my_logger.addHandler(handler)
-my_logger.debug('DESKTOP-MGFHUAM logid="0100032002" botnet user="admin" srcip=192.168.14.33 dstip=10.100.100.254 action="login"')
+my_logger.debug('DESKTOP-MGFHUAM logid="0100032002" emailfilter spam srcip=192.168.14.33 dstip=10.100.100.254 action="login"')
